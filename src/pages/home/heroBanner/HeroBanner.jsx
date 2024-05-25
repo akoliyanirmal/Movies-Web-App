2dab33c04df732cc788886a8f7472a8cb59f5356
Nirmal <akoliyanirmal43@gmail.com>
2024-05-25 18:01:57 +0530

    Create a ContentWrapper page and Update a app & HeroBanner page

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
 
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
@@ -16,7 +30,15 @@ const HeroBanner = () => {
   return (
     <>
       <div className="heroBanner">
-        <div className="wrapper">
+        {!loading && (
+          <div className="backdrop-img">
+            <img src={background} alt="" />
+          </div>
+        )}
+
+        <div className="opacity-layer"></div>
+
+        <ContentWrapper>
           <div className="heroBannerContent">
             <span className="title">Welcome.</span>
             <span className="subTitle">
@@ -32,7 +54,7 @@ const HeroBanner = () => {
               <button>Search</button>
             </div>
           </div>
-        </div>
+        </ContentWrapper>
       </div>
     </>
   );
