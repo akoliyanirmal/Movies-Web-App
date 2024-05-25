01ab914b600ca20438ca58170a4460fa0ffd3168
Nirmal <akoliyanirmal43@gmail.com>
2024-05-25 10:26:45 +0530

    Add a search functionality in HeroBanner page and Add all paths in App page

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <>
+      <div className="heroBanner">
+        <div className="wrapper">
+          <div className="heroBannerContent">
+            <span className="title">Welcome.</span>
+            <span className="subTitle">
+              Millions of Movies, TV shows and people discover. Explore now.
+            </span>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or tv show..."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeroBanner;
